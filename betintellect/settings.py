9ca2bb9f31c8b40af07672e8301aca69dad2da1e
Karina Garces ✨ <karinadgarces@gmail.com>
2023-08-21 22:21:39 -0400

    user model

--- betintellect/settings.py
+++ betintellect/settings.py
@@ -35,12 +35,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard.apps',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
