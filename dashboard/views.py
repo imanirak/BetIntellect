776b5b0ee6e72a948da52f830b4708e4d6e18b85
Karina Garces ✨ <karinadgarces@gmail.com>
2023-08-29 23:31:40 -0400

    establish connection to api

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
+import requests
+from requests import get, auth
+import environ
+env = environ.Env()
+
+
+client_id = env('client_id')
+client_secret = env('client_secret')
+a = auth.HTTPBasicAuth(client_id, client_secret)
+url = "https://api.seatgeek.com/2/taxonomies"
+res = requests.get(url, auth=a)
+data = res.json()
 
 
 def index(request):
-    return HttpResponse("Hello!")
+      
+    return HttpResponse(data)
 
 
-def baseball(request):
+def baseball(request, team):
     return HttpResponse("Baseball page")
 
 
