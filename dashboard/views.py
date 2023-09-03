ee3bc77f35e16b88d86a7e92d8301dd6b32d3a44
Karina Garces ✨ <karinadgarces@gmail.com>
2023-09-02 23:18:41 -0400

    get player id function

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from nba_api.stats.endpoints import commonplayerinfo
+from nba_api.stats.static import players
 from .models import Player
+
+
+def get_all_players():
+    all_players = players.get_players()
+    name = input() 
+    [player for player in all_players if player['full_name'] == name][0]
+    player_id = f"{name} is player"
+    return player_id
 
 
 def get_nba_player_info(player_id):
