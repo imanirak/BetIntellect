3f2e93f15634333b83f41fa2951f5ed284d2b655
Karina Garces ✨ <karinadgarces@gmail.com>
2023-09-03 13:34:20 -0400

    search bar setup

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    path("search/", views.search, name="search"),
     path("", views.dashboard, name="dashboard"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,30 +5,28 @@ from .models import Player
 
 
 def get_player_by_name(player_name: str):
+    # grab all players
     all_players = players.get_players()
+    # for every player available search for the
+    # player matching the name we provided
     found_player = [player for player in all_players
                     if player['full_name'] == player_name][0]
-    context = {
-        'player_id': found_player['id'],
-        'player_full_name': found_player['full_name'],
-    }
-
-    return print(context)
-
-
-get_player_by_name("LeBron James")
+    # grab player id for future usuage
+    player_id = found_player['id']
+    return player_id
 
 
 def get_nba_player_info(player_id):
-    player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id)
-    return player_info.get_normalized_dict()['CommonPlayerInfo'][0]
+    # grab all common player info based on provided player_id
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id)
+    player_data = player_info.get_normalized_dict()['CommonPlayerInfo'][0]
+    return player_data
 
 
 def dashboard(request):
     # Fetch NBA player data using the nba_api package
     player_id = 203999  # Replace with the actual player ID
     player_data = get_nba_player_info(player_id)
-
     # Create or update Player object in the database
     player, created = Player.objects.get_or_create(
         nba_id=player_data['PERSON_ID'],
@@ -45,3 +43,11 @@ def dashboard(request):
     return render(request, 'dashboard.html', context)
 
 
+def search(request):
+    # defines what happens when there is a POST req
+    if request.method == "POST":
+        title = request.POST.get("q")
+        return render(request, 'search_results.html', {'title': title})
+    # defines what happens when there is a GET req
+    else:
+        return render(request, 'searchbar.html')
